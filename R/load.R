a7fd8c5b119782a4d5977ebbcac0eb1f8d27435d
courtneyruss <courtneymruss@gmail.com>
2024-02-26 12:19:53 +1300

    modified function

--- R/load.R
+++ R/load.R
@@ -1,8 +1,18 @@
+#' Load data
+#' @param resource_id A string
+#' @return large list
+#'
 #' @export
 load_data <- function(resource_id) {
-url <- paste0("https://catalogue.data.govt.nz/api/3/action/datastore_search?resource_id=", resource_id, "&limit=32000")
+  url <- paste0("https://catalogue.data.govt.nz/api/3/action/datastore_search?resource_id=", resource_id)
 
-response <- httr::GET(url)
-content <- httr::content(response, "text")
-parsed <- jsonlite::fromJSON(content)
+  response <- httr::GET(url)
+
+  if (response$status_code == 200) {
+    content <- httr::content(response, "text")
+    parsed <- jsonlite::fromJSON(content)
+    parsed
+  } else {
+    stop("Error: Failed to fetch data. Status code: ", response$status_code)
+  }
 }
