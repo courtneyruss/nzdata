2d4bd5d9eaea211d1774557fcdf65e140fe52880
courtneyruss <courtneymruss@gmail.com>
2024-02-26 12:33:25 +1300

    add example

--- R/load.R
+++ R/load.R
@@ -1,10 +1,12 @@
 #' Load data
 #' @param resource_id A string
 #' @return large list
-#'
+#' @examples
+#' streets <- '2f4e30a7-091f-4aed-b18f-4b21cc19e6b4'
+#' load_data(streets)
 #' @export
 load_data <- function(resource_id) {
-  url <- paste0("https://catalogue.data.govt.nz/api/3/action/datastore_search?resource_id=", resource_id)
+  url <- paste0("https://catalogue.data.govt.nz/api/3/action/datastore_search?resource_id=", resource_id, "&limit=32000")
 
   response <- httr::GET(url)
 
